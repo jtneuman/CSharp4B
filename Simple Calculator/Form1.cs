319fd7f0a44f2d07b72e5c9ddc13ab73af3706a4
Jeff Neumann <Jeff.Neumann@phoenix.edu>
2017-08-22 09:21:44 -0400

    Refactored simple calc project - minus - div - and mult signs from word doc still might be the problem - all else working though.

--- Simple Calculator/Form1.cs
+++ Simple Calculator/Form1.cs
@@ -12,143 +12,85 @@ namespace Simple_Calculator
 {
     public partial class Form1 : Form
     {
-        const string noValue = "0";
-        const string minusSign = "-";
-        const string decimalSign = ".";
-        const string plusSign = "+";
-        const string divisionSign = "/";
-        const string multiplicationSign = "x";
-
+        #region Constants
+                const string noValue = "0";
+                const string minusSign = "-";
+                const string decimalSign = ".";
+                const string plusSign = "+";
+                const string divisionSign = "/";
+                const string multiplicationSign = "x";
+        #endregion
+        #region Variables
         string memory = String.Empty;
+        #endregion
 
-        public Form1()
+        #region Methods
+
+        private string AddDigit(string labelText, string buttonText)
         {
-            InitializeComponent();
+            if (labelText == noValue)
+                labelText = String.Empty;
+
+            labelText += buttonText;
+
+            return labelText;
         }
 
-        private void Form1_Load(object sender, EventArgs e)
+        private void DeleteDigit(ref string newValue, string value)
         {
-
-        }
-
-        private void btnNumber_Click(object sender, EventArgs e)
-        {
-            Button btn = (Button)sender;
-
-            if (lblValue.Text == noValue)
-                lblValue.Text = String.Empty;
-
-            lblValue.Text += btn.Text;
-        }
-
-        private void btnDelete_Click(object sender, EventArgs e)
-        {
-            string newValue = noValue;
-
-            int length = Convert.ToInt32(lblValue.Text.Length);
+            int length = Convert.ToInt32(value.Length);
 
             if (length > 1)
-                newValue = lblValue.Text.Substring(0, length - 1);
+                newValue = value.Substring(0, length - 1);
 
             if (newValue.Equals(minusSign))
                 newValue = noValue;
-
-            lblValue.Text = newValue;
         }
 
-        private void btnClear_Click(object sender, EventArgs e)
+        private string AddDecimal(string value)
         {
-            lblValue.Text = noValue;
-            lblFirstValue.Text = String.Empty;
-            lblMathFunction.Text = String.Empty;
-        }
-
-        private void btnMemory_Click(object sender, EventArgs e)
-        {
-            memory = lblValue.Text;
-            lblValue.Text = noValue;
-        }
-
-        private void btnMemoryRecall_Click(object sender, EventArgs e)
-        {
-            lblValue.Text = memory;
-        }
-
-        private void btnDecimal_Click(object sender, EventArgs e)
-        {
-            bool hasDecimal = lblValue.Text.Contains(decimalSign);
+            bool hasDecimal = value.Contains(decimalSign);
             if (!hasDecimal)
-                lblValue.Text += decimalSign;
+                value += decimalSign;
+
+            return value;
         }
 
-        private void btnCalculate_Click(object sender, EventArgs e)
+        private string ToggleSign(string value)
         {
-            Button btn = (Button)sender;
-
-            lblMathFunction.Text = btn.Text;
-
-            lblFirstValue.Text = lblValue.Text;
-
-            lblValue.Text = noValue;
-        }
-
-        private void btnSign_Click(object sender, EventArgs e)
-        {
-            bool hasSign = lblValue.Text.Contains(minusSign);
+            bool hasSign = value.Contains(minusSign);
 
             if (hasSign)
-                lblValue.Text = lblValue.Text.Replace(minusSign, String.Empty);
+                value = value.Replace(minusSign, String.Empty);
 
-            else if (lblValue.Text != noValue)
-                lblValue.Text = lblValue.Text.Insert(0, minusSign);
+            else if (value != noValue)
+                value = value.Insert(0, minusSign);
+
+            return value;
         }
 
-        private void btnSqrt_Click(object sender, EventArgs e)
+        private bool CanCalculate()
         {
-            bool hasSign = lblValue.Text.Contains(minusSign);
-
-            if (hasSign)
-            {
-                MessageBox.Show("Cannot calculate the square root of a negative number");
-                return;
-            }
-
-            var endsWithDecimalSign = lblValue.Text.EndsWith(decimalSign);
-
-            int length = lblValue.Text.Length;
-
-            if (endsWithDecimalSign)
-                lblValue.Text = lblValue.Text.Substring(0, length - 1);
-
-            var value = Convert.ToDouble(lblValue.Text);
-
-            var result = Math.Sqrt(value);
-
-            lblValue.Text = result.ToString();
+            return lblFirstValue.Text.Equals(String.Empty) ||
+                   lblMathFunction.Text.Equals(String.Empty) ||
+                   lblValue.Text.Equals(String.Empty) ||
+                   lblValue.Text.Equals(noValue);              
         }
 
-        private void btnEquals_Click(object sender, EventArgs e)
+        private string EndWithDecimalSign(string value)
         {
-            if (lblFirstValue.Text.Equals(String.Empty) ||
-                lblMathFunction.Text.Equals(String.Empty) ||
-                lblValue.Text.Equals(noValue))
-                return;
-            var endsWithDecimalSign = lblFirstValue.Text.EndsWith(decimalSign);
-            int length = Convert.ToInt32(lblFirstValue.Text.Length);
+            var endsWithDecimalSign = value.EndsWith(decimalSign);
+            int length = Convert.ToInt32(value.Length);
             if (endsWithDecimalSign)
-                lblFirstValue.Text = lblFirstValue.Text.Substring(0, length - 1);
+                value = value.Substring(0, length - 1);
 
-            length = Convert.ToInt32(lblValue.Text.Length);
-            endsWithDecimalSign = lblValue.Text.EndsWith(decimalSign);
-            if (endsWithDecimalSign)
-                lblValue.Text = lblValue.Text.Substring(0, length - 1);
+            return value;
+        }
 
-            var value1 = Convert.ToDouble(lblFirstValue.Text);
-            var value2 = Convert.ToDouble(lblValue.Text);
-
-            var mathFunction = lblMathFunction.Text;
-
-            var result = 0d;
+        private void Calculate(out double result, double value1, double value2,
+            string mathFunction)
+        {
+            result = 0d;
 
             if (mathFunction.Equals(plusSign))
                 result = value1 + value2;
@@ -158,12 +100,114 @@ namespace Simple_Calculator
                 result = value1 / value2;
             else if (mathFunction.Equals(multiplicationSign))
                 result = value1 * value2;
-
-            lblMathFunction.Text = String.Empty;
-            lblFirstValue.Text = String.Empty;
-
-            lblValue.Text = result.ToString("F3");
-
         }
+        #endregion
+
+        #region Constructors
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        #endregion
+
+        #region Control Events
+                private void Form1_Load(object sender, EventArgs e)
+                {
+
+                }
+
+                private void btnNumber_Click(object sender, EventArgs e)
+                {
+                    Button btn = (Button)sender;
+                    lblValue.Text = AddDigit(lblValue.Text, btn.Text);
+                }
+
+                private void btnDelete_Click(object sender, EventArgs e)
+                {
+                    string newValue = noValue;
+                    DeleteDigit(ref newValue, lblValue.Text);
+                    lblValue.Text = newValue;
+                }
+
+                private void btnClear_Click(object sender, EventArgs e)
+                {
+                    lblValue.Text = noValue;
+                    lblFirstValue.Text = String.Empty;
+                    lblMathFunction.Text = String.Empty;
+                }
+
+                private void btnMemory_Click(object sender, EventArgs e)
+                {
+                    memory = lblValue.Text;
+                    lblValue.Text = noValue;
+                }
+
+                private void btnMemoryRecall_Click(object sender, EventArgs e)
+                {
+                    lblValue.Text = memory;
+                }
+
+                private void btnDecimal_Click(object sender, EventArgs e)
+                {
+                    lblValue.Text = AddDecimal(lblValue.Text);
+                }
+
+                private void btnCalculate_Click(object sender, EventArgs e)
+                {
+                    Button btn = (Button)sender;
+
+                    lblMathFunction.Text = btn.Text;
+
+                    lblFirstValue.Text = lblValue.Text;
+
+                    lblValue.Text = noValue;
+                }
+
+                private void btnSign_Click(object sender, EventArgs e)
+                {
+                    lblValue.Text = ToggleSign(lblValue.Text);
+                }
+
+                private void btnSqrt_Click(object sender, EventArgs e)
+                {
+                    bool hasSign = lblValue.Text.Contains(minusSign);
+
+                    if (hasSign)
+                    {
+                        MessageBox.Show("Cannot calculate the square root of a negative number");
+                        return; // Exit out of the event prematurely
+                    }
+
+                    lblValue.Text = EndWithDecimalSign(lblValue.Text);
+
+                    int length = lblValue.Text.Length;
+                    var value = Convert.ToDouble(lblValue.Text);
+                    var result = Math.Sqrt(value);
+                    lblValue.Text = result.ToString();
+                }
+
+                private void btnEquals_Click(object sender, EventArgs e)
+                {
+                    if (CanCalculate()) return;
+
+                    lblFirstValue.Text = EndWithDecimalSign(lblFirstValue.Text);
+                    lblValue.Text = EndWithDecimalSign(lblValue.Text);
+
+                    var value1 = Convert.ToDouble(lblFirstValue.Text);
+                    var value2 = Convert.ToDouble(lblValue.Text);
+                    var mathFunction = lblMathFunction.Text;
+                    var result = 0d; // double =0d
+
+                    Calculate(out result, value1, value2, mathFunction);
+
+                    lblMathFunction.Text = String.Empty;
+                    lblFirstValue.Text = String.Empty;
+
+                    lblValue.Text = result.ToString("F3");
+                }
+
+        #endregion
+
     }
 }
